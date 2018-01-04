0c24da8db624d9429dee028a264150df10d84f8b
Geoff Petrie <g.petrie@gmail.com>
2018-01-04 12:28:47 -0700

    Add vim/tmux integration

--- vim-init.sh
+++ vim-init.sh
@@ -37,6 +37,7 @@ git clone git@github.com:tpope/vim-unimpaired.git
 git clone git@github.com:tpope/vim-vinegar.git
 git clone git@github.com:tpope/vim-eunuch.git
 git clone git@github.com:skalnik/vim-vroom.git
+git clone https://github.com/christoomey/vim-tmux-navigator.git
 
 git clone git@github.com:pangloss/vim-javascript.git
 git clone git@github.com:tpope/vim-markdown.git
